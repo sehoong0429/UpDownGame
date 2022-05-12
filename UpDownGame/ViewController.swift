11b9498fcc9e34767b61ef6706f6aecd41967c0d
sehoong0429 <tjsgh0801@naver.com>
2022-05-12 09:16:26 +0900

    Initial Commit

---
+++ UpDownGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by Seoyeon Hong on 2022/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
